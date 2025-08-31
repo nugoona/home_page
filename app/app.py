22427d1b40ddf275c6d135bb601ffe439863f959
Oscar <oscar@nugoona.com>
2025-09-01 08:34:04 +0900

    Fix app.py to use Cloud Run environment variables

--- app/app.py
+++ app/app.py
@@ -3,9 +3,9 @@ import os
 
 app = Flask(__name__, template_folder='../', static_folder='../static')
 
-# 정적 자산 버전 및 CDN 설정
-app.config['STATIC_VERSION'] = '1.0.0'
-app.config['STATIC_CDN_URL'] = None  # CDN 사용시 URL 설정
+# 정적 자산 버전 및 CDN 설정 - Cloud Run 환경변수에서 읽어오기
+app.config['STATIC_VERSION'] = os.environ.get('STATIC_VERSION', '1.0.0')
+app.config['STATIC_CDN_URL'] = os.environ.get('STATIC_CDN_URL')  # Cloud Run 환경변수 사용
 
 # 템플릿 전역 함수 등록
 @app.template_global()
