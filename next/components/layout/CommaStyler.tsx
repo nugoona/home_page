740d7d1703098836fbb618f97c357633bdea834b
Oscar <oscar@nugoona.com>
2026-03-23 09:03:49 +0900

    auto: workspace session save

--- next/components/layout/CommaStyler.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useCommaSerif } from '@/lib/formatText';
-
-export default function CommaStyler() {
-  useCommaSerif();
-  return null;
-}
--- next/components/layout/OuterContainer.tsx
+++
@@ -1,7 +0,0 @@
-export default function OuterContainer({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="relative max-w-[1200px] mx-auto mt-5 border border-border-default border-b-0 md:border md:border-b-0 max-md:border-none max-md:mt-0">
-      {children}
-    </div>
-  );
-}
